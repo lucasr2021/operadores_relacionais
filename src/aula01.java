1b580c8013e192e7db748689fd8d431c776bf0ca
perkles <lucasrochasilva90@hotmail.com>
2022-10-24 16:35:45 -0300

    Primeiro commit

---
+++ src/aula01.java
@@ -0,0 +1,39 @@
+public class aula01 {
+    public static void main(String[]args){
+        int i1 = 10;
+        int i2 = 20;
+        float f1 = 4.5f;
+        float f2 = 3.5f;
+        double d1 = 59.60;
+        char c1 = 'x';
+        char c2 = 'y';
+        String s1 = "Fulano";
+        String s2 = "Fulana";
+        String s3 = "Ciclano";
+        boolean b1 = true;
+        boolean b2 = false;
+
+        long l1 = 1593l;
+        long l2 = 3997l;
+        short h1 = 1;
+        byte y = 25;
+
+        System.out.println("i1 é igual a i1? " + (i1 == i1));
+        System.out.println("10 é diferente de 20? "+( 10 != 20) );
+        System.out.println("i1 é maior i2? "+ (i1 > i2) );
+        System.out.println("i1 é menor ou igual a i2 ? "+(i1 <= i2));
+
+        System.out.println("f1 é igual a f2? " + (f1 == f2));
+        System.out.println("f1 é diferente de f2? "+( f1 != f2) );
+        System.out.println("f1 é maior f2? "+ (f2 > f2) );
+        System.out.println("f1 é menor ou igual a f2 ? "+(f2 <= f1));
+
+        System.out.println("c1 é igual a c2? " + (c1 == c2));
+        System.out.println("c1 é diferente de c2? "+(c2 != c2));
+        System.out.println("c1 é maior c2? "+ (c1 > c2) );
+        System.out.println("c1 é menor ou igual a c2 ? "+(c1 <= c2));
+
+
+
+    }
+}
