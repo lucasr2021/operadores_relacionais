6089c84654ad3478e0bed00ca2b443a761c37070
perkles <lucasrochasilva90@hotmail.com>
2022-10-25 16:04:59 -0300

    commit

--- src/reolucaoExercicio.java
+++ src/reolucaoExercicio.java
@@ -6,14 +6,14 @@ public static  void main(String[]args){
     boolean b3 = true;
     boolean b4 = false;
 
-    System.out.println("b1 && b2" + (b1 && b2));
-    System.out.println("b1 && b3" + (b1 && b3));
+    System.out.println("b1 && b2 " + (b1 && b2));
+    System.out.println("b1 && b3 " + (b1 && b3));
 
-    System.out.println("b2 || b3" + (b2 || b3));
-    System.out.println("b2 || b4" + (b2 || b4));
+    System.out.println("b2 || b3 " + (b2 || b3));
+    System.out.println("b2 || b4 " + (b2 || b4));
 
-    System.out.println("b1 ^ b3" + (b1 || b3));
-    System.out.println("b2 || b4" + (b2 || b4));
+    System.out.println("b1 ^ b3 " + (b1 || b3));
+    System.out.println("b2 || b4 " + (b2 || b4));
 
     System.out.println(!b1);
     System.out.println(!b2);
