9544f42bee1f7e17c912639fb25958e9d2865f9b
perkles <lucasrochasilva90@hotmail.com>
2022-10-25 20:15:20 -0300

    commit

--- src/reolucaoExercicio.java
+++ src/reolucaoExercicio.java
@@ -17,5 +17,20 @@ public static  void main(String[]args){
 
     System.out.println(!b1);
     System.out.println(!b2);
+    
+    int i1 = 10;
+    int i2 = 5;
+    float f1 = 20f;
+    float f2 = 50f;
+
+    System.out.println("((i1 + i2) <  (f2 - f1)) && true " + ((i1 + i2) < (f2 - f1) && true));
+    System.out.println("(i1 > 12) || (f2 < f2 " + ((i1 > 12 ) || (f2 < f2 )));
+
+   // boolean salarioBaixo = salarioMensal < mediaSalario;
+    ///boolean muitosDependentes = quantidadeDependente >= poucosDependentes;
+
+    //System.out.println((salarioBaixo) && muitosDependentes);
+
+
 }
 }
