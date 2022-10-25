5b92d41691b7fd1fe2a238ac2bfb85b8f333c4c0
perkles <lucasrochasilva90@hotmail.com>
2022-10-25 16:03:39 -0300

    comiit

---
+++ src/reolucaoExercicio.java
@@ -0,0 +1,21 @@
+
+public class reolucaoExercicio {
+public static  void main(String[]args){
+    boolean b1 = true;
+    boolean b2 = false;
+    boolean b3 = true;
+    boolean b4 = false;
+
+    System.out.println("b1 && b2" + (b1 && b2));
+    System.out.println("b1 && b3" + (b1 && b3));
+
+    System.out.println("b2 || b3" + (b2 || b3));
+    System.out.println("b2 || b4" + (b2 || b4));
+
+    System.out.println("b1 ^ b3" + (b1 || b3));
+    System.out.println("b2 || b4" + (b2 || b4));
+
+    System.out.println(!b1);
+    System.out.println(!b2);
+}
+}
